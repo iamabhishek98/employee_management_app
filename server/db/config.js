5f1d44be2d82dae94ff70ce4f7c6922024957b4f
Abhishek <saiabhishek.karnati@u.nus.edu>
2021-09-09 17:24:58 +0800

    Added env variables in server

--- server/db/config.js
+++ server/db/config.js
@@ -1,8 +1,17 @@
 const Sequelize = require("sequelize");
 
-const DATABASE = process.env.NODE_ENV === "test" ? "test_postgres" : "postgres";
+const DB_HOST = process.env.DB_HOST || "host.docker.internal";
+
+const DB_USER = process.env.DB_USER || "postgres";
+
+const DB_PASSWORD = process.env.DB_PASSWORD || "postgres";
+
+const DB_PORT = process.env.DB_PORT || "5432";
+
+const DB_DATABASE =
+  process.env.NODE_ENV === "test" ? "test_postgres" : "postgres";
 
 module.exports = new Sequelize(
-  `postgresql://postgres:postgres@host.docker.internal:5432/${DATABASE}`,
+  `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
   { logging: false }
 );
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 try {
   db.authenticate()
